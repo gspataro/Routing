0d31235be1b1f1aafed594f5e725ec428af610e2
Giuseppe Spataro <spatyx@gmail.com>
2022-05-12 12:40:07 +0200

    update(Response): add 403 Forbidden status

--- src/Response.php
+++ src/Response.php
@@ -31,6 +31,7 @@ final class Response
     {
         $type = match($status) {
             200 => "200 OK",
+            403 => "403 Forbidden",
             404 => "404 Not Found",
             405 => "405 Method Not Allowed",
             default => "200 OK"
